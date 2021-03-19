115f1dc60580467fc52cfd02d76f2b6aa2397f35
rozyfauzan <rozyfauzan28@gmail.com>
2021-03-19 18:54:34 +0700

    task 3.1

---
+++ app/src/main/java/org/d3if4079/hitungbmi/data/KategoriBMI.kt
@@ -0,0 +1,5 @@
+package org.d3if4079.hitungbmi.data
+
+enum class KategoriBMI {
+    Kurus,Ideal,Gemuk
+}
---
+++ app/src/main/java/org/d3if4079/hitungbmi/ui/SaranFragment.kt
@@ -0,0 +1,58 @@
+package org.d3if4079.hitungbmi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import org.d3if4079.hitungbmi.R
+import org.d3if4079.hitungbmi.data.KategoriBMI
+import org.d3if4079.hitungbmi.databinding.FragmentSaranBinding
+
+class SaranFragment : Fragment() {
+    private lateinit var binding: FragmentSaranBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle? ): View? {
+        binding = FragmentSaranBinding.inflate(layoutInflater, container, false)
+        updateUI(KategoriBMI.Kurus)
+        return binding.root;
+    }
+
+    private fun updateUI(kategori : KategoriBMI){
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        when(kategori){
+            KategoriBMI.Kurus->{
+                actionBar?.title = getString(R.string.judul_kurus)
+                binding.gambarkondisiImageView.setImageResource(R.drawable.kurus)
+                binding.deksripsikondisiTextView.text = getString(R.string.saran_kurus)
+
+
+            }
+
+            KategoriBMI.Ideal->{
+                actionBar?.title = getString(R.string.judul_ideal)
+                binding.gambarkondisiImageView.setImageResource(R.drawable.ideal)
+                binding.deksripsikondisiTextView.text = getString(R.string.saran_ideal)
+
+
+            }
+
+            KategoriBMI.Gemuk->{
+            actionBar?.title = getString(R.string.judul_gemuk)
+            binding.gambarkondisiImageView.setImageResource(R.drawable.gemuk)
+            binding.deksripsikondisiTextView.text = getString(R.string.saran_gemuk)
+
+               }
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
